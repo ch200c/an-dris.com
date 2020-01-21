aac7e4a47d9de0c8d647faba309f10e2d284bdb7
ch200c <mantas.m@hotmail.com>
2020-01-21 20:50:11 +0000

    Add files via upload

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'AN DRIS'
+SITENAME = 'AN DRIS'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('GitHub', 'https://github.com/ch200c'),
+#          ('Stack Overflow', 'https://stackoverflow.com/users/7063763/ch200c'),)
+
+# Social widget
+#SOCIAL = (('Email', '#'),)
+SOCIAL = (('GitHub', 'https://github.com/ch200c'),
+         ('Stack Overflow', 'https://stackoverflow.com/users/7063763/ch200c'),
+         ('Email', 'mailto:an.dris.developer@gmail.com'),)
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
